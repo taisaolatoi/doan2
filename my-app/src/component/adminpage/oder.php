fb04d044b6deafc3192026e7d7aa427213c9e9c8
Tran Tan Hung <tanhung.1897@gmail.com>
2024-05-15 13:00:40 +0700

    hehe

---
+++ my-app/src/component/adminpage/oder.php
@@ -0,0 +1,14 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Lấy dữ liệu từ biểu mẫu
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $product = $_POST["product"];
+    $quantity = $_POST["quantity"];
+
+}
+    
+    
+
+
+?>
